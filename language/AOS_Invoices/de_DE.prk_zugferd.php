560b5381420705247b4012090f77ad7b2fcfafe6
Pi-R-JB <pierre.rohr@gmail.com>
2026-08-29 00:12:01 +0200

    Add service period labels

--- language/AOS_Invoices/de_DE.prk_zugferd.php
+++ language/AOS_Invoices/de_DE.prk_zugferd.php
@@ -8,3 +8,6 @@ $mod_strings['LBL_ORIGINAL_INVOICE_ID'] = 'Ursprungsrechnung ID';
 
 $mod_strings['LBL_REVENUE_AMOUNT'] = 'Umsatzwirksamer Betrag';
 $mod_strings['LBL_REVENUE_AMOUNT_USDOLLAR'] = 'Umsatzwirksamer Betrag (Standardwährung)';
+
+$mod_strings['LBL_SERVICE_PERIOD_START'] = 'Leistungsbeginn';
+$mod_strings['LBL_SERVICE_PERIOD_END'] = 'Leistungsende';
