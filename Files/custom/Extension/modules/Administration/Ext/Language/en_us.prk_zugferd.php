e470b6dcb8e01e5d1153114793ef9dc1432b7fe1
Pi-R-JB <pierre.rohr@gmail.com>
2026-08-30 12:56:48 +0200

    Add English ZUGFeRD admin labels

---
+++ Files/custom/Extension/modules/Administration/Ext/Language/en_us.prk_zugferd.php
@@ -0,0 +1,13 @@
+<?php
+
+$mod_strings['LBL_PRK_ZUGFERD_ADMIN_GROUP'] =
+    'ZUGFeRD / Factur-X';
+
+$mod_strings['LBL_PRK_ZUGFERD_ADMIN_GROUP_DESC'] =
+    'Settings for electronic invoices in ZUGFeRD / Factur-X format';
+
+$mod_strings['LBL_PRK_ZUGFERD_SETTINGS'] =
+    'ZUGFeRD Settings';
+
+$mod_strings['LBL_PRK_ZUGFERD_SETTINGS_DESC'] =
+    'Seller, tax, bank and default settings for electronic invoices';
