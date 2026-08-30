52f3581dd73d2b3c7923885ee30ad8d4ea2fbc8d
Pi-R-JB <pierre.rohr@gmail.com>
2026-08-30 12:56:41 +0200

    Add German ZUGFeRD admin labels

---
+++ Files/custom/Extension/modules/Administration/Ext/Language/de_DE.prk_zugferd.php
@@ -0,0 +1,13 @@
+<?php
+
+$mod_strings['LBL_PRK_ZUGFERD_ADMIN_GROUP'] =
+    'ZUGFeRD / Factur-X';
+
+$mod_strings['LBL_PRK_ZUGFERD_ADMIN_GROUP_DESC'] =
+    'Einstellungen für elektronische Rechnungen im ZUGFeRD-/Factur-X-Format';
+
+$mod_strings['LBL_PRK_ZUGFERD_SETTINGS'] =
+    'ZUGFeRD-Einstellungen';
+
+$mod_strings['LBL_PRK_ZUGFERD_SETTINGS_DESC'] =
+    'Verkäufer-, Steuer-, Bank- und Standardeinstellungen für elektronische Rechnungen';
